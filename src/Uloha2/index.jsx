adebe4cf640d4e4696bf3562b8a3054a70f42d3c
Filip Chalupa <chalupa.filip@gmail.com>
2021-05-08 22:44:33 +0200

    Úloha 3: odevení pozornosti od event.target.textContent
    Closes #2

--- src/Uloha2/index.jsx
+++ src/Uloha2/index.jsx
@@ -4,5 +4,5 @@ import hradUrl from './hrad.svg'
 // Zadání: Po najetí myši, `onMouseEnter` na obrázek zobraz bublinu s textem „Na hradě je myš!“
 
 export const Uloha2 = () => {
-	return <img src={hradUrl} width={60} height={60} alt="Hrad" />
+	return <img src={hradUrl} width={60} height={60} alt="hrad" />
 }
